c917be96acd695514a38a06de7f4ceac0f57e6d5
Ravi-1331 <ravimuzpatna44@gmail.com>
2025-05-03 23:16:10 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -6,17 +6,31 @@ import AppRoutes from './src/routes/index.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT
-const frontend_Url = process.env.frontend_Url
+const PORT = process.env.PORT || 5000
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 const app = express()
 
+// app.use(cors("*"))
+
+// Middleware
 app.use(cors({
-  origin: frontend_Url,
+  origin: FRONTEND_URL,
+  credentials: true
+}))
+
+// Handle pre-flight requests
+app.options('*', cors({
+  origin: FRONTEND_URL,
   credentials: true
 }))
 
 app.use(express.json())
+
+// Routes
 app.use('/', AppRoutes)
 
-app.listen(PORT, () => console.log(`App is listening ${PORT}`))
+// Start Server
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`)
+})
